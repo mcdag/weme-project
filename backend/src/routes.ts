48412458141818ea215a9e821395d4015aac8fb7
mcdag <mcdag@cin.ufpe.br>
2023-07-23 01:31:21 -0300

    fix: /user routes changed to /users

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,8 +4,8 @@ import { CredentialController } from "./controllers/CredentialController";
 
 const routes = Router();
 
-routes.post("/user", new UserController().createUser);
-routes.get("/user", new UserController().getUserLogin);
+routes.post("/users", new UserController().createUser);
+routes.get("/users", new UserController().getUserLogin);
 routes.post("/credentials", new CredentialController().createCredential);
 routes.get("/credentials", new CredentialController().getCredentials);
 routes.put("/credentials/:id", new CredentialController().updateCredential);
