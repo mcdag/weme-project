077e091e1e57018b834d36557540f9991ba2c6b2
mcdag <mcdag@cin.ufpe.br>
2023-07-23 01:11:25 -0300

    feat: route of user login /login changed to /user

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,7 +5,7 @@ import { CredentialController } from "./controllers/CredentialController";
 const routes = Router();
 
 routes.post("/user", new UserController().createUser);
-routes.get("/login", new UserController().getUserLogin);
+routes.get("/user", new UserController().getUserLogin);
 routes.post("/credentials", new CredentialController().createCredential);
 routes.get("/credentials", new CredentialController().getCredentials);
 routes.put("/credentials/:id", new CredentialController().updateCredential);
