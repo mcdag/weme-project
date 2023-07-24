41adbf35c9de624267594033416ecd475427eb13
mcdag <mcdag@cin.ufpe.br>
2023-07-24 03:20:59 -0300

    feat: cors added to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import dataSource from "./ormconfig";
 import * as dotenv from "dotenv";
 import { routes } from "./routes";
+import cors from "cors";
 dotenv.config();
 
 
@@ -13,6 +14,7 @@ dataSource.initialize()
 	
 const server = express();
 server.use(express.json());
+server.use(cors());
 server.use(routes);
 
 server.listen(process.env.SERVER_PORT, () => console.log("server is running!"));
